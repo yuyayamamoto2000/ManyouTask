8bb2c2422805e6c0f914d198e1ca0ea0fc748196
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-16 13:49:52 +0900

    Taskの詳細画面作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,19 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
