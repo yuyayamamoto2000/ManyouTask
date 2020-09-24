0274a3e5dc398c0cf653befe9224a91437fc9522
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-24 15:39:01 +0900

    controller、viewにTime_limit実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :time_limit)
   end
 
   def set_task
