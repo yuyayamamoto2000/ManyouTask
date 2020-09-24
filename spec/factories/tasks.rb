ab64bbbb30e492c3acd0d10d9fc19923d76b7c95
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-24 11:48:31 +0900

    テスト修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,8 +5,16 @@ FactoryBot.define do
   end
   # 作成するテストデータの名前を「second_task」とします
   # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
+  factory :first_task, class: Task do
+    title { 'name1' }
+    content { 'content1' }
+  end
   factory :second_task, class: Task do
-    title { 'Factoryで作ったデフォルトのタイトル２' }
-    content { 'Factoryで作ったデフォルトのコンテント２' }
+    title { 'name2' }
+    content { 'content2' }
+  end
+  factory :third_task, class: Task do
+    title { 'name3' }
+    content { 'content3' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,30 +1,13 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do#describeには、「何の仕様についてなのか」
-  let!(:task){ FactoryBot.create(:task, title: 'task') }
-  let!(:second_task){ FactoryBot.create(:second_task) }
-  before do
-    # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
-    visit tasks_path
-  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do #contextには「状況・状態を分類」したテスト内容
       it '作成したタスクが表示される' do#itには「期待する動作」を記載します。
-        # 1. new_task_pathに遷移する（新規作成ページに遷移する）
         visit new_task_path
-        # 2. 新規登録内容を入力する
         _task = Task.new
-        #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
-        # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in 'task[title]', with: 'タスク名'
-        # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in 'task[content]',with: 'タスク詳細'
-        # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
-        # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
         click_on 'commit'
-        # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-        # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-        # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）をexpect確認（期待）するコードを書く
-        # binding.irb
         expect(page).to have_content 'タスク名'
         expect(page).to have_content 'タスク詳細'
       end
@@ -34,7 +17,7 @@ RSpec.describe 'タスク管理機能', type: :system do#describeには、「何
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task)
+        _task = FactoryBot.create(:task, title: 'task')
         # タスク一覧ページに遷移
         visit tasks_path
         # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
@@ -45,11 +28,17 @@ RSpec.describe 'タスク管理機能', type: :system do#describeには、「何
     end
 
     context 'タスクが作成日時の降順に並んでる場合' do
+      before do
+        FactoryBot.create(:first_task)
+        FactoryBot.create(:second_task)
+        FactoryBot.create(:third_task)
+      end
       it '新しいタスクが一番上に表示される' do
         #タスク一覧ページに遷移
         visit tasks_path
-         task_list = all('td')
-         expect(task_list[0]).to have_content 'Factoryで作ったデフォルトのタイトル２'
+        task_list = all('td.task_row')
+        binding.pry
+        expect(task_list[0]).to have_content 'name3'
       end
     end
   end
