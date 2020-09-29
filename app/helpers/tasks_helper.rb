8fc28a99d430961e1e3e8299c5ad619b935ec41d
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-09-29 22:38:09 +0900

    動作確認、細かい添削

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,7 @@
 module TasksHelper
   def confirm_new_or_edit
     if action_name == 'new' || action_name == 'create'
-      confirm_tasks_path
+      tasks_path
     elsif action_name == 'edit' || action_name == 'update'
       #confirm_task_path(@task.id)
       task_path
