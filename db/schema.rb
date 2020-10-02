c9fafe47c336f162293379de49d26011fb9336e9
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-02 15:44:39 +0900

    Userモデル、テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_082825) do
+ActiveRecord::Schema.define(version: 2020_10_02_064018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2020_09_15_082825) do
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.string "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "time_limit", default: -> { "now()" }, null: false
+    t.integer "priority", default: 0
+    t.integer "to_do", default: 0
+    t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
