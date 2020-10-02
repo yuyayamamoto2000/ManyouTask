25c230f041a002fab5e16fa6c1d0f67788671811
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-02 16:19:51 +0900

    userの詳細ページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
