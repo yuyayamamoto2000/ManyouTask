f78eebedb4e02c788b1b61d8c6448869ba050883
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-02 15:48:38 +0900

    emailのカラムにインデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_064018) do
+ActiveRecord::Schema.define(version: 2020_10_02_064721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_064018) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
