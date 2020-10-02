fd71cd5b3e8ea970488f41da0fe927ffa2c2401f
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-02 18:56:21 +0900

    sessionsのルート定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   resources :users
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
