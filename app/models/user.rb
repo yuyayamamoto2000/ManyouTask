f4cddc898164148fd47fc1b1dfcc94f279b2b8ee
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-02 16:04:35 +0900

    パスワード機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   beffore_validation { email.downcase! }
+  has_secure_password
 end
