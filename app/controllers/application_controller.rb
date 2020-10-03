49fbdc982b3191531d21243ca2f7fb3ccc3a5d86
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-03 19:14:13 +0900

    ログインしていないユーザーを強制的にログイン画面に飛ばす処理実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
- include SessionsHelper
+  include SessionsHelper
+  def authenticate_user
+    # 現在ログイン中のユーザが存在しない場合、ログインページにリダイレクトさせる。
+    if @current_user == nil
+      flash[:notice] = t('notice.login_needed')
+      redirect_to new_session_path
+    end
+  end
 end
