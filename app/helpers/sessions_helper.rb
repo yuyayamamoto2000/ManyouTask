3d47f97b095358adc8980d1a929090dc8246533f
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-03 19:12:47 +0900

    ログイン、ログアウト機能実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def current_user
+   @current_user ||= User.find_by(id: session[:user_id]) #@current_userが真の場合はそのままにし、偽の場合は右辺の値User.find_by(id: session[:user_id])を代入すると言う意味。
+  end
+
+  def logged_in?#ユーザーがログインしていればtrue、その他ならfalseを返すメソッドを定義
+    current_user.present?
+  end
 end
