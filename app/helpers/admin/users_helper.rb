3e6d0019d1ce9834f53749afd355897510d75924
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-06 17:51:31 +0900

    管理権限仮実装

---
+++ app/helpers/admin/users_helper.rb
@@ -0,0 +1,10 @@
+module Admin::UsersHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      admin_users_path
+    elsif action_name == 'edit' || action_name == 'update'
+      #binding.pry
+      admin_users_path
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_111159) do
+ActiveRecord::Schema.define(version: 2020_10_03_123231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_10_03_111159) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
